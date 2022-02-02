b4a627fbf84d0f10123ade926bd33b28781d710b
INTEGRISOFT\dana.sarghe <dana.sarghe@integrisoft.ro>
2022-02-02 12:10:26 +0200

    added components recipe&shopping

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { RecepiesComponent } from './recepies/recepies.component';
+import { RecepiesListComponent } from './recepies/recepies-list/recepies-list.component';
+import { RecepeDetailComponent } from './recepies/recipe-detail/recipe-detail.component';
+import { RecepeItemComponent } from './recepies/recepies-list/recipe-item/recipe-item.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
+import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    RecepiesComponent,
+    RecepiesListComponent,
+    RecepeDetailComponent,
+    RecepeItemComponent,
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
